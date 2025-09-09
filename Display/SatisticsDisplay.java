67b08d52d58d1ac9071b1bccd8a5acbc7238ac20
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-09-08 21:02:12 -0500

    Update SatisticsDisplay.java

--- Display/SatisticsDisplay.java
+++ Display/SatisticsDisplay.java
@@ -4,10 +4,35 @@
  */
 package Display;
 
-/**
- *
- * @author Estudiantes
- */
-public class SatisticsDisplay {
-    
+import weatherstation.observer.Observer;
+import weatherstation.subject.Subject;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum = 0.0f;
+    private int numReadings;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        tempSum += temp;
+        numReadings++;
+
+        if (temp > maxTemp) maxTemp = temp;
+        if (temp < minTemp) minTemp = temp;
+
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Promedio/Max/Min = " 
+            + (tempSum / numReadings) + "/" + maxTemp + "/" + minTemp);
+    }
 }
