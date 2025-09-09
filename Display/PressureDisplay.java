53874c8de740f271a6055d691bb11f80a876bec5
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-09-09 08:04:16 -0500

    Update PressureDisplay.java

--- Display/PressureDisplay.java
+++ Display/PressureDisplay.java
@@ -1,13 +1,29 @@
-/*
+e/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Display;
 
-/**
- *
- * @author Estudiantes
- */
-public class PressureDisplay {
-    
+import weatherstation.observer.Observer;
+import weatherstation.subject.Subject;
+
+public class PressureDisplay implements Observer, DisplayElement {
+    private float pressure;
+    private Subject weatherData;
+
+    public PressureDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.pressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Presión atmosférica actual: " + pressure + " hPa");
+    }
 }
