5e650c42bcf51aa637a91651195e3025eae64539
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-09-09 08:01:08 -0500

    Update ForecastDisplay.java

--- Display/ForecastDisplay.java
+++ Display/ForecastDisplay.java
@@ -4,10 +4,35 @@
  */
 package Display;
 
-/**
- *
- * @author Estudiantes
- */
-public class ForecastDisplay {
-    
+import weatherstation.observer.Observer;
+import weatherstation.subject.Subject;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+    private float currentPressure = 1013.25f;  
+    private float lastPressure;
+    private Subject weatherData;
+
+    public ForecastDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        lastPressure = currentPressure;
+        currentPressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.print("Pronóstico: ");
+        if (currentPressure > lastPressure) {
+            System.out.println("El clima mejorará!");
+        } else if (currentPressure == lastPressure) {
+            System.out.println("El mismo clima.");
+        } else {
+            System.out.println("Se acerca lluvia.");
+        }
+    }
 }
